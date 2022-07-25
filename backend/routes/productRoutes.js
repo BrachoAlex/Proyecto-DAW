069cc7efafffca9d956e5f0f3d872f600bea6848
BrachoAlex <99853881+BrachoAlex@users.noreply.github.com>
2022-07-25 16:39:54 -0500

    Initial

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {getAllProducts, getProductById} = require("../controller/productControllers");
+
+//GET ALL ITEMS/PRODUCTS FROM DB
+//ROUTE: /api/products
+
+router.get('/', getAllProducts);
+
+//GET SINGLE ITEM/PRODUCT BY ID FROM DB
+//ROUTE: /api/products/:id
+
+router.get('/',getProductById);
+
+module.exports = router;
---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home";
+import UpperBody from "./components/UpperBody";
+import LowerBody from "./components/LowerBody";
+import Accesories from "./components/Accesories";
+import Footwear from "./components/Footwear";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return   ( //<div className="app">This is working!</div>;
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = { <Home />}  />
+        <Route path="/lower-body" element = { <LowerBody />}  />
+        <Route path="/upper-body" element = { <UpperBody />}  />
+        <Route path="/footwear" element = { <Footwear />}  />
+        <Route path="/accesories" element = { <Accesories />}  />
+        
+
+
+      </Routes>
+    </BrowserRouter>
+  );
+  
+}
+
+export default App;
