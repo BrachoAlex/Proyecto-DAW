b998b41b0e1ef33f28e24d2363ef577d1e197079
Carlos Silva <carlos.silvap@outlook.com>
2022-07-26 22:22:20 -0500

    footer

--- frontend/src/components/Footwear.js
+++ frontend/src/components/Footwear.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from "./Navbar";
-
+import Footer from "./Footer";
 
 export default function Footwear() {
   return (
     <div>
       <Navbar/>
       <h1>Footwear</h1>
+      <Footer/>
     </div>
   )
 }
--- frontend/src/components/LowerBody.js
+++ frontend/src/components/LowerBody.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function LowerBody() {
   return (
     <div>
       <Navbar/>
       <h1>LowerBody</h1>
+      <Footer/>
     </div>
   )
 }
