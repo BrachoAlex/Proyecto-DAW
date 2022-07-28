1874550a95b9478d2e8a4d17386f44bda07a123e
BrachoAlex <99853881+BrachoAlex@users.noreply.github.com>
2022-07-28 13:31:29 -0500

    Final Routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,7 +13,23 @@ router.get("/getAllProducts", async (req, res) =>{
         res.status(500).json({message: "SERVER ERROR"});
     }
 });
+router.post("/addProduct", async(req,res) =>{
+    let product = new Product(req.body);
+    
+    await product.save();
+    res.json(product);
+});
 
+router.delete("/deleteProduct",async(req,res) =>{
+    let product =await Product.findById(req.body.id)
+    if (product){
+        await product.delete()
+        res.json({msg: "PRODUCT DELETED"})
+    }
+    else {
+        res.json({msg: "COULD NOT DELETE PRODUCT"})
+    }
+});
 module.exports = router;
 
 
